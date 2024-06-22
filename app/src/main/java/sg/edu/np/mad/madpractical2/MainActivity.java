902ef50ff1fb2e1faf51e801fcf2c2f6f9058d5c
Brian Wong <s10257346@connect.np.edu.sg>
2024-06-22 17:00:22 +0800

    Test 4

--- app/src/main/java/sg/edu/np/mad/madpractical2/MainActivity.java
+++ app/src/main/java/sg/edu/np/mad/madpractical2/MainActivity.java
@@ -1,39 +1,46 @@
 package sg.edu.np.mad.madpractical2;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+    User user;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        user = new User("John Doe", "Android Developer", 1, false);
+
+        TextView nameTextView = findViewById(R.id.nameTextView);
+        TextView descriptionTextView = findViewById(R.id.descriptionTextView);
+        Button followButton = findViewById(R.id.followButton);
+
+        nameTextView.setText(user.name);
+        descriptionTextView.setText(user.description);
+
+        updateFollowButton(followButton);
+
+        followButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                user.followed = !user.followed;
+                updateFollowButton(followButton);
+            }
         });
-        User user = new User("John Doe", "MAD Developer", 1, false);
+    }
 
-        //Get the TextViews and Button from the layout
-        TextView tvName = findViewById(R.id.tvName);
-        TextView tvDescription = findViewById(R.id.tvDescription);
-        Button btnFollow = findViewById(R.id.btnFollow);
-
-        //Set the TextViews with the User's name, description and default button message
-
-        tvName.setText(user.name);
-        tvDescription.setText(user.description);
-        btnFollow.setText("Follow");
-
+    private void updateFollowButton(Button followButton) {
+        if (user.followed) {
+            followButton.setText("Unfollow");
+        } else {
+            followButton.setText("Follow");
+        }
     }
 }
